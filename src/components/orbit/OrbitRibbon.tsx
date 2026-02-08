da50b2b1de09d5d960dd80a339350a73ae70027c
brett-the-jet21 <brettdeavila@gmail.com>
2026-02-08 05:38:04 +0000

    Fix OrbitRibbon TypeScript error

--- src/components/orbit/OrbitRibbon.tsx
+++ src/components/orbit/OrbitRibbon.tsx
@@ -1,12 +1,16 @@
 import * as THREE from "three";
+import { Line } from "@react-three/drei";
 
 export function OrbitRibbon({ from, to, colorA, colorB, opacity }: any) {
   const points = [new THREE.Vector3(...from), new THREE.Vector3(...to)];
-  const geometry = new THREE.BufferGeometry().setFromPoints(points);
   
   return (
-    <line geometry={geometry}>
-      <lineBasicMaterial color={colorA} transparent opacity={opacity} linewidth={2} />
-    </line>
+    <Line
+      points={points}
+      color={colorA}
+      lineWidth={2}
+      transparent
+      opacity={opacity}
+    />
   );
 }
